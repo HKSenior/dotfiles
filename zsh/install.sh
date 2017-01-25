0d8541fa636574e58b56bfe2f8eab0acd8bbd40f
Hassani <hassanisnr@gmail.com>
2017-01-25 14:00:08 -0500

    Fixed the naming issue of oh-my-zsh, corrected zsh/install.sh & added scriptArt to repo

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,8 +14,10 @@ alias="$PWD/alias.zsh"
 theme1="$PWD/my-theme.zsh-theme"
 theme2="$PWD/old-theme.zsh-theme"
 
+
 # Print the current directory
 echo -e "You are in: ${BLUE}$PWD${NC}"
+
 
 # Check if the required files are there
 if [ -e "$alias" ]; then
@@ -39,17 +41,20 @@ else
     ANY_MISSING=true
 fi
 
+
 # If a file is missing exit
 if [ "$ANY_MISSING" = true ]; then
     echo -e "${RED}One or more of the needed files are missing.${NC}"
     exit 1
 fi
 
+
 # Set flag to false
 ANY_MISSING=false
 
+
 # Create symbolic links & check exit code for each link
-ln -sf "$alias" "$PWD/.oh_my_zsh/lib/alias.zsh"
+ln -sf "$alias" "$PWD/.oh-my-zsh/lib/alias.zsh"
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}alias.zsh is linked${NC}" 
 else
@@ -57,9 +62,7 @@ else
     ANY_MISSING=true
 fi
 
-
-ln -sf "$theme1" "$PWD/.oh_my_zsh/themes/my-theme.zsh-theme"
-# Check exit code
+ln -sf "$theme1" "$PWD/.oh-my-zsh/themes/my-theme.zsh-theme"
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}my-theme.zsh-theme is linked${NC}" 
 else
@@ -67,15 +70,14 @@ else
     ANY_MISSING=true
 fi
 
-
-ln -sf "$theme2" "$PWD/.oh_my_zsh/themes/old-theme.zsh-theme"
-# Check exit code
+ln -sf "$theme2" "$PWD/.oh-my-zsh/themes/old-theme.zsh-theme"
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}old-theme.zsh-theme is linked${NC}" 
 else
     echo -e "${RED}old-theme.zsh-theme isn't linked${NC}"
     ANY_MISSING=true
 fi
+
 
 # Check if the links were successful
 if [ "$ANY_MISSING" = true ]; then

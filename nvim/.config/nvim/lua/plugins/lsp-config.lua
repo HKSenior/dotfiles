0961ab749091e31f87fa6f117241fdf4239a2efc
hksenior <hassanisnr@gmail.com>
2024-11-02 12:40:30 -0400

    Ignore sentence capitalization when using harper_ls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -25,7 +25,13 @@ return {
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
       lspconfig.ts_ls.setup({})
-      lspconfig.harper_ls.setup({})
+      lspconfig.harper_ls.setup {
+        settings = {
+          ["harper-ls"] = {
+            sentence_capitalization = false,
+          }
+        }
+      }
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
